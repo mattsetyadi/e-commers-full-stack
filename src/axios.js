28c0556a6a5586ce34d53078d6ef46f204e042fd
mattsetyadi <matt.musicianstuff@gmail.com>
2021-01-01 12:38:40 +0700

    finished

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/ms-store-23819/us-central1/api",
+  baseURL: 'https://us-central1-ms-store-23819.cloudfunctions.net/api',
+  // baseURL: 'http://localhost:5001/ms-store-23819/us-central1/api',
   //THE API (cloud function) URL
 });
 
